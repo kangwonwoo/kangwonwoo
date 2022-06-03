8585bcae328163ccab7d677bd7adcec2c9bc7d2c
kww9496@gmail.com <kww9496@gmail.com>
2022-06-03 17:15:20 +0900

    01단계

---
+++ Backjoon/01단계/Main1_04.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backjoon._01단계
+{
+    internal class Main1_04
+    {
+        static void Main4(string[] args)
+        {
+        }
+    }
+}
