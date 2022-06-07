602c3cf15f46247874e5f38cfd6b48c0065e6e44
kww9496@gmail.com <kww9496@gmail.com>
2022-06-07 12:19:02 +0900

    2022/06/07 수업예제

--- Ch03/4_While.cs
+++ Ch03/4_While.cs
@@ -13,7 +13,7 @@ namespace Ch03
 {
     internal class _4_While
     {
-        static void Main(string[] args)
+        static void Main4(string[] args)
         {
             // 1부터 10까지 합
             int sum = 0;
