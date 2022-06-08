a7ca7b17d28c1ee6612ee4e367b0dd8a1ae92b44
kww9496@gmail.com <kww9496@gmail.com>
2022-06-08 17:16:53 +0900

    교재예제

---
+++ Book/Ch05/p241.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Ch05
+{
+    internal class p241
+    {
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+
+        [STAThread]
+        static void Main241(string[] args)
+        {
+            //Application.EnableVisualStykes();
+            //Application.SetCimpatibleTextRenderingDefault(false)
+               // Application.Run(new Form1());
+        }
+    }
+}
