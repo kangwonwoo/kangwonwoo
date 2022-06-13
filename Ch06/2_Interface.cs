3e7d83f3eb3888cd64f421ad95539c5f2591963a
kww9496@gmail.com <kww9496@gmail.com>
2022-06-13 13:19:50 +0900

    2022/06/13 수업예제

--- Ch06/2_Interface.cs
+++ Ch06/2_Interface.cs
@@ -9,6 +9,11 @@ using System.Threading.Tasks;
  * 날짜 : 2022/06/13
  * 이름 : 강원우
  * 내용 : 인터페이스 실습 교재 p425
+ * 
+ * 인터페이스(Interface)
+ * - 클래스간의 공통의 통일된 표준 규격을 통해 클래스 구조를 설계하는 문법
+ * - 인터페이스를 활용해서 다중 상속을 구현
+ * - 인터페이스를 활용해서 객체간 결합도를 완화 하는 효과를 얻는다.(다형성) 
  */
 namespace Ch06
 {
@@ -30,12 +35,19 @@ namespace Ch06
             samsung.SoundDown();
             samsung.PowerOff();
 
-            // 인터페이스를 활용한 다중 상속
-            // 다중 상속은 불가능하나 인터페이스를 활용하면 가능해진다.
+            // 인터페이스를 활용한 다중 상속 -> 다형성 사용 불가 (사용시 결과 값 에러)
+            // C#에서 일반적으로 다중 상속은 불가능하나 인터페이스를 활용하면 가능해진다.
             TV tv = new TV();
             tv.PowerOn();
             tv.Booting();
             tv.Access();
+
+            // 인터페이스를 활용한 결합도 완화(다형성)
+            Bulb bulb = new Bulb();
+            ISocket socket = new Cable(bulb);
+
+            socket.SwitchOn();
+            socket.SwitchOff();
         }
     }
 }
