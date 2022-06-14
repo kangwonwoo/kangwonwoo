2f4b523a3b7145e73705788e6c535150bb810621
kww9496@gmail.com <kww9496@gmail.com>
2022-06-14 11:17:02 +0900

    2022/06/14 수업예제

--- Ch06/4_Generic.cs
+++ Ch06/4_Generic.cs
@@ -9,10 +9,13 @@ using System.Threading.Tasks;
  * 날짜 : 2022/06/14
  * 이름 : 강원우
  * 내용 : 제네릭 실습하기 교재 p395
+ * 
+ * 제네릭(Generic)
+ * - 클래스 내부의 변수의 타입을 동적으로 일반화 시키는 문법
+ * - 제네릭 사용으로 클래스를 범용성을 갖도록 활용
  */
 namespace Ch06
 {
-    
     class FruitBox <T>
     {
         private T fruit;
@@ -27,7 +30,7 @@ namespace Ch06
 
     internal class _4_Generic
     {
-        static void Main(string[] args)
+        static void Main4(string[] args)
         {
             Apple apple = new Apple("한국", 3000);
             Banana banana = new Banana("대만", 4000);
