22f107415a7f4faca2a605819f2c44ec256bed89
kww9496@gmail.com <kww9496@gmail.com>
2022-06-15 10:21:33 +0900

    교재예제

--- Book/Ch09/p440.cs
+++ Book/Ch09/p440.cs
@@ -8,5 +8,22 @@ namespace Book.Ch09
 {
     internal class p440
     {
+        class program
+        {
+            class Parent { }
+
+            class Child : Parent, IDisposable, IComparable  // 한 개의 클래스와 두 개의 인터페이스를 상속받는다.
+            {
+                public void Dispose()
+                {
+                    throw new NotImplementedException();
+                }
+
+                public int CompareTo(object? obj)
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
     }
 }
